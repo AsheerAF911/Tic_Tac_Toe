e8b067e561eaa6ad229ef4482fe44632d19ca89b
AsheerAF911 <alamfarooqia@gmail.com>
2023-04-28 11:03:21 +0530

    final styling

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,9 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-orange">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -78,8 +81,9 @@ function App() {
       >
         New Game
       </button>
-      <h2>Current Game History</h2>
+      <h2 style={{ fontWeight: 'normal' }}>Current Game History</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls"></div>
     </div>
   );
 }
